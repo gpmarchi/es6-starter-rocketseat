678b9586b3f3e1aaf8b5b909cc400cfc66c5f2ef
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-08-29 15:55:40 -0300

    app final para listagem de repositórios do github

--- src/main.js
+++ src/main.js
@@ -1,29 +1,98 @@
-import ClasseUsuario, { idade as IdadeUsuario } from "./functions";
-import { umPorSegundo } from "./functions";
-import { umPorSegundoAsync } from "./functions";
-import { getUserFromGithub } from "./functions";
-import { getUserFromGithubAsync } from "./functions";
-import { Github } from "./functions";
-import { buscaUsuario } from "./functions";
-import { buscaUsuarioAsync } from "./functions";
+import api from "./api";
 
-// console.log(ClasseUsuario.info());
-// console.log(IdadeUsuario);
+class App {
+  constructor() {
+    this.repositories = [];
+    this.formElem = document.querySelector("form#repo-form");
+    this.inputTextElem = document.querySelector("input#repo-name");
+    this.listElem = document.querySelector("ul#repo-list");
+    this.registerHandlers();
+  }
 
-// umPorSegundo();
-// umPorSegundoAsync();
+  registerHandlers() {
+    this.formElem.onsubmit = event => this.addRepository(event);
+  }
 
-// getUserFromGithub("diego3g");
-// getUserFromGithub("diego3g124123");
+  setLoading(loading = true) {
+    if (loading) {
+      let loadingElem = document.createElement("span");
+      loadingElem.appendChild(document.createTextNode("Carregando..."));
+      loadingElem.setAttribute("id", "loading");
 
-// getUserFromGithubAsync("diego3g");
-// getUserFromGithubAsync("diego3g124123");
+      this.formElem.appendChild(loadingElem);
+    } else {
+      document.querySelector("form#repo-form span#loading").remove();
+    }
+  }
 
-// Github.getRepositories("gpmarchi", "credit-card-validator");
-// Github.getRepositories("rocketseat/dslkvmskv");
+  async addRepository(event) {
+    event.preventDefault();
 
-// Github.getRepositoriesAsync("gpmarchi", "credit-card-validator");
-// Github.getRepositoriesAsync("rocketseat", "dslkvmskv");
+    const repoName = this.inputTextElem.value;
 
-// buscaUsuario("diego3g");
-buscaUsuarioAsync("diego3g");
+    if (repoName.length === 0) {
+      return;
+    }
+
+    this.setLoading();
+
+    try {
+      //desestruturação de objetos
+      const response = await api.get(`/repos/${repoName}`);
+
+      //object short syntax - variáveis com o mesmo nome não precisam ser
+      //atribuídas, pois são mapeadas automaticamente
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url }
+      } = response.data;
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+
+      this.inputTextElem.value = " ";
+
+      this.render();
+    } catch (error) {
+      alert("O repositório não existe!");
+    }
+
+    this.setLoading(false);
+  }
+
+  render() {
+    this.listElem.innerHTML = "";
+
+    this.repositories.forEach(repo => {
+      let imgElem = document.createElement("img");
+      imgElem.setAttribute("src", repo.avatar_url);
+
+      let titleElem = document.createElement("strong");
+      titleElem.appendChild(document.createTextNode(repo.name));
+
+      let descriptionElem = document.createElement("p");
+      descriptionElem.appendChild(document.createTextNode(repo.description));
+
+      let linkElem = document.createElement("a");
+      linkElem.setAttribute("target", "_blank");
+      linkElem.setAttribute("href", repo.html_url);
+      linkElem.appendChild(document.createTextNode("Acessar"));
+
+      let listItemElem = document.createElement("li");
+      listItemElem.appendChild(imgElem);
+      listItemElem.appendChild(titleElem);
+      listItemElem.appendChild(descriptionElem);
+      listItemElem.appendChild(linkElem);
+
+      this.listElem.appendChild(listItemElem);
+    });
+  }
+}
+
+new App();
