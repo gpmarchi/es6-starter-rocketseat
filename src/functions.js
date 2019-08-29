f183feb37ecb93aab2199d90a8b9ac3acabc7b16
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-08-29 13:53:21 -0300

    desafios async/await

--- src/functions.js
+++ src/functions.js
@@ -2,6 +2,98 @@ export const idade = 38;
 
 export default class Usuario {
   static info() {
-    console.log("Apenas teste");
+    return "Apenas teste";
   }
 }
+
+// Função delay aciona o .then após 1s
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+export function umPorSegundo() {
+  delay().then(() => {
+    console.log("1s");
+    delay().then(() => {
+      console.log("2s");
+      delay().then(() => {
+        console.log("3s");
+      });
+    });
+  });
+}
+
+export async function umPorSegundoAsync() {
+  await delay();
+  console.log("1s");
+  await delay();
+  console.log("2s");
+  await delay();
+  console.log("3s");
+}
+
+// Usando axios para o request
+import axios from "axios";
+export function getUserFromGithub(user) {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(err => {
+      console.log("Usuário não existe");
+    });
+}
+
+export async function getUserFromGithubAsync(user) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`);
+    console.log(response.data);
+  } catch (error) {
+    console.warn("Usuário não existe");
+  }
+}
+
+// Usando axios dentro de uma classe com método estático
+export class Github {
+  static getRepositories(user, repo) {
+    axios
+      .get(`https://api.github.com/repos/${user}/${repo}`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
+
+  static async getRepositoriesAsync(user, repo) {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/repos/${user}/${repo}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.warn(error.message);
+    }
+  }
+}
+
+// Utilização com constante e arrow functions
+export const buscaUsuario = usuario => {
+  axios
+    .get(`https://api.github.com/users/${usuario}`)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(err => {
+      console.log("Usuário não existe");
+    });
+};
+
+export const buscaUsuarioAsync = async usuario => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${usuario}`);
+    console.log(response.data);
+  } catch (error) {
+    console.warn(error.message);
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import ClasseUsuario, { idade as IdadeUsuario } from "./functions";
+import { umPorSegundo } from "./functions";
+import { umPorSegundoAsync } from "./functions";
+import { getUserFromGithub } from "./functions";
+import { getUserFromGithubAsync } from "./functions";
+import { Github } from "./functions";
+import { buscaUsuario } from "./functions";
+import { buscaUsuarioAsync } from "./functions";
 
-ClasseUsuario.info();
-console.log(IdadeUsuario);
+// console.log(ClasseUsuario.info());
+// console.log(IdadeUsuario);
+
+// umPorSegundo();
+// umPorSegundoAsync();
+
+// getUserFromGithub("diego3g");
+// getUserFromGithub("diego3g124123");
+
+// getUserFromGithubAsync("diego3g");
+// getUserFromGithubAsync("diego3g124123");
+
+// Github.getRepositories("gpmarchi", "credit-card-validator");
+// Github.getRepositories("rocketseat/dslkvmskv");
+
+// Github.getRepositoriesAsync("gpmarchi", "credit-card-validator");
+// Github.getRepositoriesAsync("rocketseat", "dslkvmskv");
+
+// buscaUsuario("diego3g");
+buscaUsuarioAsync("diego3g");
